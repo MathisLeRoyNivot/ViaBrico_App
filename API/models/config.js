2c07cf9a228a2e21caa7e6b106796e7729dbd741
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-05-07 13:32:58 +0200

    Delete password and host data

--- API/models/config.js
+++ API/models/config.js
@@ -1,10 +1,10 @@
 // Config file
 
 // MySQL Db config
-const dbHost = 'nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-const dbUsername = 'w3dqhcf5479yml68';
-const dbPassword = 'bdwztxowdz2cy3gs';
-const dbName =  'owxbsewog8grckyt';
+const dbHost = '<dbHost>';
+const dbUsername = '<dbUsername>';
+const dbPassword = '<dbPassword>';
+const dbName =  '<dbName>';
 const dbTableUsers = 'compte';
 const dbTableProviders = 'fournisseur';
 
